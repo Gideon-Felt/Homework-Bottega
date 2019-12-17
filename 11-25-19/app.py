cb6c79962992b319da2632ffb8cbb22151a656eb
Gideon-Felt <gideon@feltslanding.com>
2019-12-16 17:30:56 -0700

    updated 11-25-19 to enable front end flask template and rebuilt DB awith new fields

--- 11-25-19/app.py
+++ 11-25-19/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -13,44 +13,53 @@ ma = Marshmallow(app)
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=False)
-    rating = db.Column(db.String(144), unique=False)
     year = db.Column(db.String(144), unique=False)
-    producer = db.Column(db.String(144), unique=False)
-    director = db.Column(db.String(144), unique=False)
+    rating = db.Column(db.String(144), unique=False)
+    genre = db.Column(db.String(144), unique=False)
+    starring = db.Column(db.String(144), unique=False)
+    
 
 
-    def __init__(self, title, rating, year, producer, director):
+    def __init__(self, title, year, rating, genre, starring):
         self.title = title
-        self.rating = rating
         self.year = year
-        self.producer = producer
-        self.director = director
+        self.rating = rating
+        self.genre = genre
+        self.starring = starring
 
 class MovieSchema(ma.Schema):
     class Meta:
-        fields = ('title', 'rating', 'year', 'producer', 'director')
+        fields = ('title', 'year', 'rating', 'genre', 'starring')
 
 movie_schema = MovieSchema()
 movies_schema = MovieSchema(many=True)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/all_movies')
+def all_entries():
+    return render_template('all_movies.html')
+
+
 # Endpoint to create a new Movie
-@app.route('/movie', methods=["POST"])
+@app.route('/add_movie', methods=["POST"])
 def add_movie():
-    title = request.json['title']
-    rating = request.json['rating']
-    year = request.json['year']
-    producer = request.json['producer']
-    director = request.json['director']
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        rating = request.form['rating']
+        genre = request.form['genre']
+        starring = request.form['starring']
 
-    new_movie = Movie(title, rating, year, producer, director)
+        new_movie = Movie(title, year, rating, genre, starring)
 
-    db.session.add(new_movie)
-    db.session.commit()
+        db.session.add(new_movie)
+        db.session.commit()
 
-    movie = Movie.query.get(new_movie.id)
-
-    return movie_schema.jsonify(movie)
-
+        return render_template('message_sent.html')
+    return home()
 
 # Endpoint to query all movies
 @app.route('/movies', methods=["GET"])
@@ -71,16 +80,16 @@ def get_movie(id):
 def movie_update(id):
     movie = Movie.query.get(id)
     title = request.json['title']
-    rating = request.json['rating']
     year = request.json['year']
-    producer = request.json['producer']
-    director = request.json['director']
+    rating = request.json['rating']
+    genre = request.json['genre']
+    starring = request.json['starring']
 
     movie.title = title
-    movie.rating = rating
     movie.year = year
-    movie.producer = producer
-    movie.director = director
+    movie.rating = rating
+    movie.genre = genre
+    movie.starring = starring
 
     db.session.commit()
     return movie_schema.jsonify(movie)
